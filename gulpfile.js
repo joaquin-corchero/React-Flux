55dca010c01a9dd992000f56fa1211da23bfcc3e
Joaquin Corchero <joaquin.corchero@gmail.com>
2016-10-06 12:09:52 +0100

    Added gulp task to automatically reload
    Signed-off-by: Joaquin Corchero <joaquin.corchero@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -35,4 +35,8 @@ gulp.task('html', function(){
   .pipe(connect.reload());//reload using connect
 });
 
-gulp.task('default', ['html', 'open']);
+gulp.task('watch', function(){
+  gulp.watch(config.paths.html, ['html']);
+});
+
+gulp.task('default', ['html', 'open', 'watch']);
